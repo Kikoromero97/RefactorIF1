6c0a9ffcfba1e109257a0a832549e986d9d0efc3
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-15 10:24:42 +0200

    Tercer Commit - Cambiado Switch por for.

--- src/DataType.java
+++ src/DataType.java
@@ -1,14 +1,31 @@
+import java.util.ArrayList;
+
 public class DataType {
     public static void main(String[] args) {
         System.out.println(DataType.gd(20));
         System.out.println(DataType.gd(702));
         System.out.println(DataType.gd(0));
     }
+
     public static String gd(int code) {
-        return switch (code) {
-            case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> "DATE";
-            case 20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396 -> "NUMERIC";
-            default -> "STRING";
-        };
+        int[] casoDate = {702, 1082, 1083, 1114, 1184, 1266, 12403};
+        int[] casoDate2 = {20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396};
+        String codigo = "";
+        for (int k : casoDate) {
+            if (k == code) {
+                codigo = "DATE";
+                break;
+            } else {
+                for (int i : casoDate2) {
+                    if (i == code) {
+                        codigo = "NUMERIC";
+                        break;
+                    } else {
+                        codigo = "STRING";
+                    }
+                }
+            }
+        }
+        return codigo;
     }
 }
